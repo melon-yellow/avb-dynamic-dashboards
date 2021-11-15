714e5ad09c4334850081d5a2a901ce8cb8c3b03c
anthony <78743473+anthony-freitas@users.noreply.github.com>
2021-11-15 13:14:00 -0300

    sdfsdfsdf

--- dyn/methods.php
+++ dyn/methods.php
@@ -528,8 +528,10 @@ function url() {
 
 //timestamp
 function timestamp(int $t=null): int {
-    if ($t !== null){
+    if ($t !== null) {
+        echo($t);
         $t = str_split((string)$t);
+        echo($t);
         return mktime(
             num($t[8].$t[9]),
             num($t[10].$t[11]),
