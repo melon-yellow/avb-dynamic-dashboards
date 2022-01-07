7f887d8328e2e573674f638857d3e53a2adcd900
Avbot <92833124+avb-git@users.noreply.github.com>
2022-01-07 19:51:37 -0300

    Update main.php

--- dyn/dash/in/lamf/main.php
+++ dyn/dash/in/lamf/main.php
@@ -5,7 +5,7 @@ require_once(str_repeat("../",substr_count($_SERVER["SCRIPT_NAME"],"/")-2)."meth
 //main
 function main(){
     //route action
-    if (@$_REQUEST["action"] == "route") { return route("Metas Laminação a Frio"); };
+    if (@$_REQUEST["action"] == "route") { return route("Metas Trefila"); };
     //update action
     if (@$_REQUEST["action"] == "update") { return update(300); };
     //if no action was found
